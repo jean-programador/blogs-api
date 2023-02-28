11a0da35c8fae1434a69240c57eee89a2268ea0f
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-02-28 16:38:26 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const app = require('./app');
+
+// não remova a variável `API_PORT` ou o `listen`
+const port = process.env.API_PORT || 3000;
+
+app.listen(port, () => console.log('ouvindo porta', port));
